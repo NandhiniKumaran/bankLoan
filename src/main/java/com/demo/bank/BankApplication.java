2b29361adad5492f452678a0be253c1a2c8f4e90
NandhiniKumaran <nandhuu.nandy@gmail.com>
2023-04-17 11:11:36 +0800

    Deployment changes

--- src/main/java/com/demo/bank/BankApplication.java
+++ src/main/java/com/demo/bank/BankApplication.java
@@ -4,10 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class BankApplication {
+public class BankApplication extends SpringBootServletInitializer {
+
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        return application.sources(BankApplication.class);
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
