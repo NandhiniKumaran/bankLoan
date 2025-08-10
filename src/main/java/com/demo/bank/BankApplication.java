7ce2b6649ed9f200741de23252e854bfdbad3acd
NandhiniKumaran <nandhuu.nandy@gmail.com>
2025-08-10 10:04:54 +0800

    test

--- src/main/java/com/demo/bank/BankApplication.java
+++ src/main/java/com/demo/bank/BankApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//Test this
 @SpringBootApplication
 
 public class BankApplication extends SpringBootServletInitializer {
